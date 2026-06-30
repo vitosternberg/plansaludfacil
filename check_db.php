2c2adfe72d494cbf037f0cf02dd0d3d89a9f7d10
Vito Sternberg <primagen@gmail.com>
2026-06-29 23:21:25 -0400

    Fix check_db.php

--- check_db.php
+++ check_db.php
@@ -4,7 +4,7 @@ $conn = connect_db_simple();
 if (!$conn) {
     die(json_encode(["error" => "No DB connection"]));
 }
-$res = $conn->query("SELECT * FROM procesar_formularios ORDER BY id_formulario_tipo DESC LIMIT 5");
+$res = $conn->query("SELECT * FROM procesar_formularios ORDER BY id DESC LIMIT 5");
 if (!$res) {
     die(json_encode(["error" => $conn->error]));
 }
