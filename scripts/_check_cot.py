ae118693db4a8ade058a2c2ed63f751cad355743
Vito Sternberg <primagen@gmail.com>
2026-07-20 18:15:37 -0400

    chore: limpiar scripts de scraping y archivos temporales

--- scripts/_check_cot.py
+++
@@ -1,8 +0,0 @@
-import json
-with open('/tmp/cot127.json') as f:
-    d = json.load(f)
-for r in d['recomendaciones'][:5]:
-    s = r['score']
-    reasons = [x for x in r.get('razones', []) if 'cobertura' in x.lower() or 'Cobertura' in x]
-    flag = 'COV' if reasons else 'NOCOV'
-    print(f"{s}/100 {flag:6s} | {r['isapre']:15s} {r['nombre'][:45]}")
