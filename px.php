3717fb1b7e9af80047474bb129327cba7e77ed08
Vito Sternberg <primagen@gmail.com>
2026-07-23 16:01:23 -0400

    debug: px.php — test sin 'tracker' en nombre

---
+++ px.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: image/gif');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Pragma: no-cache');
+echo base64_decode('R0lGODlhAQABAJAAAP8AAAAAACH5BAUQAAAALAAAAAABAAEAAAICRAEAOw==');
+exit;
